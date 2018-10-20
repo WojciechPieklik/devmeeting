1a9220e48fd95ca2dad13321aa1ebb30e04e301f
Wojciech Pieklik <wojtek.pieklik@gmail.com>
2018-10-20 11:04:21 +0200

    [#1.3] uzycie cli

---
+++ devmeeting/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Najlepsze języki programowania"/>
+    <DevMeeting />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import DevMeeting from './components/DevMeeting.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    DevMeeting,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ devmeeting/src/components/DevMeeting.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="l in languages">{{l.name}}</li>
+    </ol>
+    <input v-model="newName" type="text" placeholder="language name">
+    <button v-on:click="addLanguage()">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DevMeeting',
+  data() {
+    return {
+      languages: [
+        {id:0, name:'Java'},
+        {id:1, name:'Java w wersji skrypt'},
+        {id:2, name:'React'}
+      ],
+      counter: 3,
+      newName: null
+    }
+  },
+  methods: {
+    addLanguage() {
+      var i = this.counter++;
+      this.languages.push({
+        id: i,
+        name: this.newName
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
